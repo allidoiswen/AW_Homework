06d303897d9cf4bd221c9255cbdb0e6f859a9c6b
allidoiswen <austinwen@gmail.com>
2020-03-23 00:12:43 -0400

    Update main.py

--- 03-Python/Homework/python-challenge/PyBank/main.py
+++ 03-Python/Homework/python-challenge/PyBank/main.py
@@ -6,14 +6,15 @@ import csv
 
 # define variables
 budget_data = os.path.join('../..', 'PyBank', 'Resources', 'budget_data.csv')
+output_date = os.path.join('PyBank_output.csv')
 
-# Read csv
+# Open and Read csv
 with open(budget_data) as bd:
     bd_reader = csv.reader(bd, delimiter = ',')
 
     # Skip header
     bd_header = next(bd_reader)
-    print(f'header row is {bd_header}')
+    ###print(f'header row is {bd_header}')
 
     # initialize variable values
     num_month = 0
@@ -53,15 +54,28 @@ with open(budget_data) as bd:
             greatest_loss = this_row_pnl
             greatest_loss_month = row[0]
 
-    ave_pnl_delta = sum(all_monthly_pnl_delta) / (num_month-1)
+ave_pnl_delta = sum(all_monthly_pnl_delta) / (num_month-1)
+ave_pnl_delta = round(ave_pnl_delta, 2)
 
-    print(num_month)
-    print(pnl)
-    print(ave_pnl_delta)
-    print(f'Greatest Increase in Profits: {greatest_profit_month} ({greatest_profit})')
-    print(f'Greatest Decrease in Profits: {greatest_loss_month} ({greatest_loss})')
+output_dict = {
+    0 : 'Financial Analysis',
+    1 : '----------------------------',
+    2 : f'Total Months: {num_month}',
+    3 : f'Average  Change: ${ave_pnl_delta}',
+    4 : f'Greatest Increase in Profits: {greatest_profit_month} (${greatest_profit})',
+    5 : f'Greatest Decrease in Profits: {greatest_loss_month} (${greatest_loss})',
+}
 
-   
+# Print Output onto Terminal
+for line in output_dict:
+    print(output_dict[line])
 
+# Save Output as a txt
+with open(output_date, 'w') as outputfile:
 
-        
+    # Initialize csv.writer
+    csvwriter = csv.writer(outputfile, delimiter=':')
+
+    # Save Each Line into the output file
+    for line in output_dict:
+        csvwriter.writerow(output_dict[line].split(':'))
