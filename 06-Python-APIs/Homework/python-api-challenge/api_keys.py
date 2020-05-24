8b03d302a6326e7ac130501615b6c560a9813052
allidoiswen <austinwen@gmail.com>
2020-05-23 20:44:22 -0400

    upload

---
+++ 06-Python-APIs/Homework/python-api-challenge/api_keys.py
@@ -0,0 +1,6 @@
+# OpenWeatherMap API Key
+# weather_api_key = "b581c9bbca7e8a61179d4ebbedf75d51"
+weather_api_key = "2ee791302a25f3fa5be6b06a6aa91e29"
+
+# Google API Key
+g_key = "AIzaSyCcmeLdmdIntDog7rx9kiPbHWUTFJliYKs"
