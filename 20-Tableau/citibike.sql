abe6d3579a38ae152e371a7d1e6df440a16e951c
allidoiswen <austinwen@gmail.com>
2020-07-18 23:24:46 -0400

    push init 20-Tableau

---
+++ 20-Tableau/citibike.sql
@@ -0,0 +1,20 @@
+CREATE TABLE tripdata (
+	tripduration      		INTEGER,
+	starttime         		TIMESTAMP, 
+	stoptime          		TIMESTAMP, 
+	startstationid    		INTEGER,
+    startstationname  		VARCHAR(100), 
+	startstationlatitude    NUMERIC(12, 8),
+	startstationlongitude	NUMERIC(12, 8),
+    endstationid            INTEGER,
+	endstationname          VARCHAR(100),
+	endstationlatitude      NUMERIC(12, 8),
+	endstationlongitude     NUMERIC(12, 8),
+	bikeid                  INTEGER,
+	usertype                VARCHAR(25),
+	birthyear               INTEGER,
+	gender                  VARCHAR(10)
+);
+
+SELECT * FROM tripdata
+LIMIT 30;
